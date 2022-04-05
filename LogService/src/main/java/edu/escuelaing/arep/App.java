2b1051529897f4ecad58275bffecf8748a8e329d
eduardoospina <eduardo.ospina@mail.escuelaing.edu.co>
2022-04-05 13:03:07 -0500

    creacion s y key

--- LogService/src/main/java/edu/escuelaing/arep/App.java
+++ LogService/src/main/java/edu/escuelaing/arep/App.java
@@ -1,13 +1,31 @@
 package edu.escuelaing.arep;
 
+import static spark.Spark.*;
+
 /**
  * Hello world!
  *
  */
 public class App 
 {
-    public static void main( String[] args )
-    {
-        System.out.println( "Hello World!" );
+    public static void main(String[] args) {
+        secure(getKeystore(), "123456",null, null);
+
+        port(getPort());
+        get("/hello", (req, res) -> "Hello world");
+    }
+
+    static int getPort() {
+        if (System.getenv("PORT") != null) {
+            return Integer.parseInt(System.getenv("PORT"));
+        }
+        return 5000; //returns default port if heroku-port isn't set(i.e. on localhost)
+    }
+
+    static String getKeystore() {
+        if (System.getenv("KEYSTORE") != null) {
+            return System.getenv("Keystore");
+        }
+        return "Keystores/ecikeystore.p12"; //returns default port if heroku-port isn't set(i.e. on localhost)
     }
 }
